8f324ebd18961172847c04529ebd0265b9346a74
objektwerks <objektwerks@runbox.com>
2024-02-05 05:12:21 -0500

    enhanced chart

--- src/main/scala/objektwerks/Chart.scala
+++ src/main/scala/objektwerks/Chart.scala
@@ -2,19 +2,19 @@ package objektwerks
 
 import java.io.File
 
-import org.jfree.chart.{ChartFactory, ChartRenderingInfo, JFreeChart}
+import org.jfree.chart.{ChartFactory, ChartRenderingInfo}
 import org.jfree.chart.entity.StandardEntityCollection
 import org.jfree.chart.util.ExportUtils
 import org.jfree.chart.imagemap.{ToolTipTagFragmentGenerator, URLTagFragmentGenerator}
 import org.jfree.data.general.DefaultPieDataset
+import org.jfree.chart.imagemap.ImageMapUtils
 
 object Chart:
-  def build(): JFreeChart =
+  def build(): String =
     val dataset = new DefaultPieDataset[String]()
     dataset.setValue("Pale Ale", 30.0)
     dataset.setValue("IPA", 60.0)
     dataset.setValue("DIPA", 10.0)
-    
     val chart = ChartFactory.createPieChart("Beer Styles", dataset, true, true, true)
 
     val file = File("chart.png")
@@ -27,6 +27,4 @@ object Chart:
     val url = new URLTagFragmentGenerator() {
                 override def generateURLFragment(arg: String) = arg
               }
-
-
-    chart
+    ImageMapUtils.getImageMap("image-map-chart", info, tooltip, url)
