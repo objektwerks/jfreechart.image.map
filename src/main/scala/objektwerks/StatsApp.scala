633eeeee28351763759d2112c668cbca2d5ff021
objektwerks <objektwerks@runbox.com>
2024-02-05 04:03:13 -0500

    refactored stats app

--- src/main/scala/objektwerks/StatsApp.scala
+++ src/main/scala/objektwerks/StatsApp.scala
@@ -1,16 +1,14 @@
 package objektwerks
 
-import com.typesafe.scalalogging.LazyLogging
-
 import scalafx.application.JFXApp3
 
-object StatsApp extends JFXApp3 with LazyLogging:
+object StatsApp extends JFXApp3:
   override def start(): Unit =
     val view = StatsView()
     stage = new JFXApp3.PrimaryStage {
       scene = view.scene
-      title = "Stats"
+      title = "JfreeChart Image Map"
     }
-    logger.info("*** Stats app started.")
+    println("*** JFreeChart Image Map app started.")
 
-  override def stopApp(): Unit = logger.info("*** Stats app stopped.")
+  override def stopApp(): Unit = println("*** JFreeChart Image Map app stopped.")
