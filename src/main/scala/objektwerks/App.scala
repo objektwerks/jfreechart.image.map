eb2f40c029ba4c1e9824c62c83c564f4914a37b5
objektwerks <objektwerks@runbox.com>
2024-02-05 04:06:21 -0500

    edited app

--- src/main/scala/objektwerks/App.scala
+++ src/main/scala/objektwerks/App.scala
@@ -4,7 +4,7 @@ import scalafx.application.JFXApp3
 
 object App extends JFXApp3:
   override def start(): Unit =
-    val view = StatsView()
+    val view = View()
     stage = new JFXApp3.PrimaryStage {
       scene = view.scene
       title = "JfreeChart Image Map"
