48e19c7c7d03882a4e75fc20d117e350439e0ca7
objektwerks <objektwerks@runbox.com>
2024-02-05 04:36:25 -0500

    edited image map chart

--- src/main/scala/objektwerks/ImageMapChart.scala
+++ src/main/scala/objektwerks/ImageMapChart.scala
@@ -1,6 +1,14 @@
 package objektwerks
 
-import org.jfree.chart.JFreeChart
+import org.jfree.chart.{ChartFactory, JFreeChart}
+import org.jfree.data.general.DefaultPieDataset
 
 object ImageMapChart:
-  def build(): JFreeChart = ???
+  def build(): JFreeChart =
+    val dataset = new DefaultPieDataset[String]()
+    dataset.setValue("Pale Ale", 30.0)
+    dataset.setValue("IPA", 60.0)
+    dataset.setValue("DIPA", 10.0)
+    
+    val chart = ChartFactory.createPieChart("Soft Drink Pie Chart", dataset, true, true, true)
+    chart
