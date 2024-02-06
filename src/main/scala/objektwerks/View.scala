a5e253bc571e976fdfd5de326d347ca2d9a20e3e
objektwerks <objektwerks@runbox.com>
2024-02-06 08:50:07 -0500

    added image view to view

--- src/main/scala/objektwerks/View.scala
+++ src/main/scala/objektwerks/View.scala
@@ -1,12 +1,21 @@
 package objektwerks
 
+import java.io.FileInputStream
+
 import scalafx.geometry.Insets
 import scalafx.scene.Scene
+import scalafx.scene.image.{Image, ImageView}
 import scalafx.scene.layout.VBox
 import scalafx.scene.web.WebView
 
+
 final class View:
   val chartInfo = Chart.build()
+
+  val file = FileInputStream(chartInfo.imageUrl)
+  val image = Image(file)
+  val imageView = ImageView(image) // Works!
+
   val webView = WebView()
   webView.getEngine().load( chartInfo.imageUrl ) // Fails to load!
   // ImageMap is empty! webView.getEngine().loadContent( chartInfo.imageMap, "text/html" )
@@ -14,7 +23,7 @@ final class View:
   val rootPane = new VBox:
     spacing = 6
     padding = Insets(6)
-    children = List(webView)
+    children = List(imageView, webView)
 
   val scene = new Scene:
     root = rootPane
