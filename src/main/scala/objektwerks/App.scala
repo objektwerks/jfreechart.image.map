e63d438f47631b808f47fede17d65bd7ad947b48
objektwerks <objektwerks@runbox.com>
2024-02-06 13:36:53 -0500

    edited app

--- src/main/scala/objektwerks/App.scala
+++ src/main/scala/objektwerks/App.scala
@@ -8,4 +8,6 @@ object App extends JFXApp3:
     stage = new JFXApp3.PrimaryStage {
       scene = view.scene
       title = "JfreeChart Image View & Map"
+      maxWidth = 612
+      maxHeight = 800
     }
